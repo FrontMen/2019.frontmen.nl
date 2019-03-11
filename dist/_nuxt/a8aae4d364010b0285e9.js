a3282d7fe994f1301e1a5ad77d45c3dc5a4aec98
Sander de Jong <sander@frontmen.nl>
2019-03-11 12:09:43 +0100

    Remove dist

--- dist/_nuxt/a8aae4d364010b0285e9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{166:function(t,e,n){"use strict";n.r(e);var s={},i=n(22),c=Object(i.a)(s,function(){this.$createElement;this._self._c;return this._m(0)},[function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"container"},[e("h2",[this._v("Welcome")])])}],!1,null,null,null);e.default=c.exports}}]);
