09470ca0ff08adef6d6228c4a16f8458ea44f8d4
Zakaria <zakaria.t@live.nl>
2019-03-18 10:18:59 +0100

    medium section styling

--- components/MediumSection.vue
+++ components/MediumSection.vue
@@ -1,22 +1,35 @@
 <template>
-  <section>
-    <h4>Frontmen on Medium</h4>
-    <h3>Art of debugging with Chrome DevTools</h3>
-    <a href="https://medium.com/frontmen/art-of-debugging-with-chrome-devtools-ab7b5fd8e0b4">
-      <img src="https://cdn-images-1.medium.com/max/2000/1*K2Utn78MaVzlUeIagOEZRg.png">
-    </a>
+  <div>
+    <section class="section">
+      <h3 class="header">Frontmen on Medium</h3>
+      <div class="content">
+        <a href="https://medium.com/frontmen/art-of-debugging-with-chrome-devtools-ab7b5fd8e0b4">
+          <img
+            class="image"
+            src="https://cdn-images-1.medium.com/max/2000/1*K2Utn78MaVzlUeIagOEZRg.png"
+          >
+        </a>
 
-    <p>
-      Chrome DevTools come with an array of features that help developers debug their apps effectively,
-      and therefore find and fix the bugs faster. There are a lot of power tools in devtools which are not quite well-known amongst the developers.
-    </p>
-    <div :style="{ textAlign: 'center', width: '100%'}">
-      <a href="https://medium.com/frontmen/art-of-debugging-with-chrome-devtools-ab7b5fd8e0b4">
-        <button>Read the full article</button>
-      </a>
-    </div>
+        <aside>
+          <h4 class="title">Art of debugging with Chrome DevTools</h4>
+
+          <p>
+            Chrome DevTools come with an array of features that help developers debug their apps effectively,
+            and therefore find and fix the bugs faster. There are a lot of power tools in devtools which are not quite well-known amongst the developers.
+          </p>
+          <div class="button-wrapper">
+            <a
+              href="https://medium.com/frontmen/art-of-debugging-with-chrome-devtools-ab7b5fd8e0b4"
+            >
+              <button class="button">Read the full article</button>
+            </a>
+          </div>
+        </aside>
+      </div>
+    </section>
+
     <div class="triangle-divider"/>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -26,19 +39,38 @@ export default {
 </script>
 
 <style scoped>
-section {
+.section {
   background-color: var(--lightblue);
 }
 
-img {
+.header {
+  background-color: var(--darkblue);
+  padding: 1rem;
+  margin: 0;
+  color: white;
+  display: inline-block;
+  text-transform: uppercase;
+  margin-bottom: 2rem;
+}
+
+.title {
+  color: white;
+  margin-top: .5rem; /* for alignment with medium image*/
+}
+
+.content {
+  display: flex;
+}
+
+.button-wrapper {
+  text-align: right;
+}
+
+.image {
   max-width: 100%;
 }
 
-h3 {
-  color: white;
-}
-
-button {
+.button {
   padding: 12px 24px;
   background: var(--orange);
   color: #fff;
@@ -53,12 +85,24 @@ button {
     border-left: 50vw solid transparent;
     border-right: 50vw solid transparent;
   }
+  .header {
+    width: 100%;
+  }
+  .content {
+    padding: 0 2rem;
+  }
 }
 
 @media (min-width: 961px) {
   .triangle-divider {
     border-left: 480px solid transparent;
     border-right: 480px solid transparent;
+  }
+  .header {
+    width: 50%;
+  }
+  .content {
+    padding: 0 8rem;
   }
 }
 
